2cf1c80a33f5ad5f42f350fa9d003f31f2d933c4
Maya Ilieva <ilieva1maya@gmail.com>
2025-11-05 15:40:57 +0200

    App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newItem, setNewItem] = useState("");
 
   return (
     <>
       <h1>DataRay...</h1>
+      <form className="basic-form" action="">
+        <div className="form-row">
+          <label htmlFor="urlInput">Enter Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            placeholder="Enter Item here..."
+            id="inpuItem"
+          />
+        </div>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+      <ul>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="complete">Complete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="complete">Complete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 3
+          </label>
+          <button className="complete">Complete</button>
+        </li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
