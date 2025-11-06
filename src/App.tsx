bf1edc39401787ddd22f30e58a795d069fe60b25
Maya Ilieva <ilieva1maya@gmail.com>
2025-11-06 11:35:14 +0200

    Add project functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,87 @@
 import { useState } from "react";
 import "./App.css";
+import type { ProjectItem } from "./interfaces/project";
 
 function App() {
-  const [newItem, setNewItem] = useState("");
+  const [newProjectTitle, setNewProjectTitle] = useState("");
+  const [projects, setProjects] = useState<ProjectItem[]>([]);
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (newProjectTitle.trim() === "") return;
+    console.log(newProjectTitle);
+
+    setProjects((prevIProjects) => {
+      const newProject: ProjectItem = {
+        id: crypto.randomUUID(),
+        title: newProjectTitle,
+        completed: false,
+      };
+      return [...prevIProjects, newProject];
+    });
+
+    setNewProjectTitle("");
+  }
+
+  function toggleProject(id: string, completed: boolean) {
+    setProjects((currentProjects) => {
+      return currentProjects.map((project) => {
+        if (project.id === id) {
+          return { ...project, completed };
+        }
+        return project;
+      });
+    });
+  }
+
+  function deleteProject(id: string) {
+    setProjects((currentProjects) => {
+      return currentProjects.filter((project) => project.id !== id);
+    });
+  }
 
   return (
     <>
-      <h1>DataRay...</h1>
-      <form className="basic-form" action="">
+      <h1>DataRay Project Tracker</h1>
+      <form onSubmit={handleSubmit} className="basic-form" action="">
         <div className="form-row">
-          <label htmlFor="urlInput">Enter Item</label>
+          <label htmlFor="inputProject">Enter Project</label>
           <input
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
+            value={newProjectTitle}
+            onChange={(e) => setNewProjectTitle(e.target.value)}
             type="text"
-            placeholder="Enter Item here..."
-            id="inpuItem"
+            placeholder="Enter Project here..."
+            id="inputProject"
           />
         </div>
         <button className="btn" type="submit">
-          Submit
+          Add Project
         </button>
       </form>
+      <h2>Current Projects: ({projects.length})</h2>
       <ul>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="complete">Complete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="complete">Complete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 3
-          </label>
-          <button className="complete">Complete</button>
-        </li>
+        {projects.length === 0 && (
+          <li className="empty">Get started by adding a project above!</li>
+        )}
+        {projects.map((project) => (
+          <li key={project.id} className={project.completed ? "completed" : ""}>
+            <label className="project-list">
+              <input
+                type="checkbox"
+                checked={project.completed}
+                onChange={(e) => toggleProject(project.id, e.target.checked)}
+                className="completed"
+              />
+              <span className="is-completed">{project.title}</span>
+            </label>
+            <button
+              onClick={() => deleteProject(project.id)}
+              className="deleted"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
       </ul>
     </>
   );
---
+++ src/interfaces/project.ts
@@ -0,0 +1,5 @@
+export interface ProjectItem {
+  id: string;
+  title: string;
+  completed: boolean;
+}
