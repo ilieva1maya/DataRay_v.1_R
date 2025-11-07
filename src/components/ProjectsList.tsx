68d3088a9f81d38c40ea1f28b1ae1759b8b66cc7
Maya Ilieva <ilieva1maya@gmail.com>
2025-11-07 14:37:28 +0200

    Fine tune

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -2,7 +2,7 @@ import type { ProjectItemI } from "../interfaces/project";
 import { ProjectItem } from "./ProjectItem";
 
 
-export function ProjectsList({projects, toggleProject, finishProject}: {
+export function ProjectsList({ projects, toggleProject, finishProject }: {
   projects: ProjectItemI[];
   toggleProject: (id: string, completed: boolean) => void;
   finishProject: (id: string) => void;
@@ -12,12 +12,12 @@ export function ProjectsList({projects, toggleProject, finishProject}: {
       {projects.length === 0 && (
         <li className="empty">Get started by adding a project above!</li>
       )}
-{projects.map((project) => ProjectItem(
-  project.completed,
-  finishProject,
-  project,
-  toggleProject
-))}
+      {projects.map((project) => ProjectItem(
+        project.completed,
+        finishProject,
+        project,
+        toggleProject
+      ))}
     </ul>
   );
 }
